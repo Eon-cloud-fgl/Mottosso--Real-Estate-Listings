9bbc51c6a955d993c567608325b82c87f1b63582
Ferchoch0 <delvalle.fernando.d@gmail.com>
2025-07-23 21:48:21 -0300

    feat: diseño ux/ui del home terminado; aun sin logica

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,63 @@
 import "../styles/home.css";
+import { GoChevronRight } from "react-icons/go";
 
 function Banner() {
     return (
-        <div className="contenedor-titulo-encabezado">
-            <p>Propiedades <br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mottoso</p>
+        <div className="hero-container">
+            <img src="/banner/home-banner--b1.jpg" className="hero-background" />
+
+            <div className="hero-content">
+                <h1 className="hero-title">Encontrar tu nuevo hogar es sencillo</h1>
+                <p className="hero-subtitle">
+                    propiedadesmottoso.com es su destino ideal para encontrar la casa de alquiler perfecta para satisfacer sus necesidades..
+                </p>
+                <div className="hero-search">
+                    <input type="text" placeholder="Encontrar tu Hogar" disabled />
+                    <button>Ir <GoChevronRight /></button>
+                </div>
+            </div>
+
+            <div className="hero-form-bar">
+                <input type="text" placeholder="Ciudad" />
+                <select>
+                    <option>Tipo</option>
+                    <option>Compra</option>
+                    <option>Apartamento</option>
+                </select>
+                <button>Buscar</button>
+            </div>
         </div>
     );
 }
 
-function Description(){
+function Description() {
     return (
         <div className="contenedor-descripcion">
+            <ImageItem />
             <DescriptionItem />
         </div>
     );
 }
 
-function DescriptionItem(){
+function DescriptionItem() {
     return (
         <div className="cd-item">
             <h6>Tu lugar, nuestra prioridad</h6>
             <p>En Mottoso Propiedades te ayudamos a encontrar el lugar ideal para vivir o invertir. Con asesoramiento profesional y una amplia variedad de opciones, hacemos que tu próxima propiedad esté más cerca.</p>
         </div>
     );
+}
+
+function ImageItem() {
+    return (
+        <div className="image-stack">
+            <img src="/info-banner-1.avif" alt="Imagen 1" className="stack-img img-1" />
+            <img src="/img/home-visual-1.jpg" alt="Imagen 2" className="stack-img img-2" />
+            <img src="/img/home-visual-2.jpg" alt="Imagen 3" className="stack-img img-3" />
+            <img src="/img/home-visual-3.jpg" alt="Imagen 4" className="stack-img img-4" />
+        </div>
+    );
+
 }
 
 function MisItems({ imageN, title, info }) {
